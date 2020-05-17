9726f2fcc21c06ca1a7fd2009eab7145eba42fce
Tan Duong <tan.duong@vexere.com>
2020-05-17 19:41:19 +0700

    Attempt to do cursor based pagination

--- src/search/ProductSearch.ts
+++ src/search/ProductSearch.ts
@@ -19,6 +19,26 @@ export type SearchOptions = {
   sort?: SortOptions[];
 };
 
+
+export function buildCursor(id: string, price: number): string {
+  let data = `${id}||${price}`;
+  let buff = new Buffer(data);
+  return buff.toString('base64');
+}
+
+export function parseCursor(cursor: string): { id: string; price?: number } {
+  let buff = new Buffer(cursor, 'base64');
+  let text = buff.toString('ascii');
+  const [id, price] = text.split('||');
+  console.log('id, price', id, price);
+
+  if (price) {
+    return { id, price: parseInt(price) };
+  }
+
+  return { id };
+}
+
 export function buildESQuery(indexConfig, options: SearchOptions): RequestParams.Search {
   const query: RequestParams.Search = {
     index: indexConfig.name,
@@ -56,26 +76,38 @@ export function buildESQuery(indexConfig, options: SearchOptions): RequestParams
     }
   }
 
+  // This short commings need a bit more considerations.
+  // The limit param alone can support pagination just fine in the first iteration.
+  // if (options.afterCursor) {
+  //   const { id, price } = parseCursor(options.afterCursor);
+  // }
+
   query.body['size'] = options.limit;
 
   if (options.sort && options.sort.length > 0) {
-    query.body['sort'] = options.sort.map(({ field, asc }) => ({ [field]: asc ? 'asc' : 'desc' }))
+    query.body['sort'] = [...options.sort.map(({ field, asc }) => ({ [field]: asc ? 'asc' : 'desc' })), { id: 'asc' }]
   }
 
   return query;
 }
 
-export function parseESResponse(results): ProductDocument[] {
+export function parseESResponse(results): { pageInfo?: { cursor: string }, data: ProductDocument[] } {
   try {
-    return results.body.hits.hits.map((h) => h._source);
+    const data = results.body.hits.hits.map((h) => h._source) as ProductDocument[];
+    const lastItem = data[data.length - 1];
+    return {
+      pageInfo: {
+        cursor: buildCursor(lastItem.id, lastItem.price)
+      },
+      data: data
+    }
   } catch (e) {
     console.error('error', e);
-    return [];
+    return { data: [] };
   }
 }
 
-
-export async function searchProduct(client: Client, options: SearchOptions): Promise<ProductDocument[]> {
+export async function searchProduct(client: Client, options: SearchOptions): Promise<{ pageInfo?: { cursor: string }, data: ProductDocument[] }> {
   return new Promise((resolve) => {
     const query = buildESQuery(IndexConfig, options)
     console.debug('Querying with', JSON.stringify(query, null, 2))
--- src/test/integration/ProductSearch.test.ts
+++ src/test/integration/ProductSearch.test.ts
@@ -1,4 +1,4 @@
-import { searchProduct } from '../../search/ProductSearch';
+import { searchProduct, buildCursor, parseCursor } from '../../search/ProductSearch';
 import { Client } from '@elastic/elasticsearch';
 
 describe('searchProduct', () => {
@@ -16,8 +16,8 @@ describe('searchProduct', () => {
     });
 
     console.log('results', results);
-    expect(results.length).toBeGreaterThan(0);
-    const foundItemNames = results.map((i) => i.name);
+    expect(results.data.length).toBeGreaterThan(0);
+    const foundItemNames = results.data.map((i) => i.name);
 
     const fishItemNames = ['Refined Steel Fish', 'Gorgeous Concrete Fish'];
 
@@ -32,9 +32,8 @@ describe('searchProduct', () => {
       sort: [],
     });
 
-    // console.log('results', results);
-    expect(results.length).toBeGreaterThan(0);
-    const foundItemNames = results.map((i) => i.name);
+    expect(results.data.length).toBeGreaterThan(0);
+    const foundItemNames = results.data.map((i) => i.name);
 
     const fishItemNames = ['Refined Plastic Fish'];
 
@@ -48,13 +47,11 @@ describe('searchProduct', () => {
       sort: [],
     });
 
-    // console.log('results', results);
-    expect(results.length).toBeGreaterThan(0);
-    const foundItemNames = results.map((i) => i.name);
+    expect(results.data.length).toBeGreaterThan(0);
+    const foundItemNames = results.data.map((i) => i.name);
 
     const fishItemNames = ['Refined Concrete Ball', 'Refined Steel Fish'];
 
-    // console.log('foundItemNames', foundItemNames);
     expect(foundItemNames).toEqual(fishItemNames);
   });
 
@@ -70,11 +67,60 @@ describe('searchProduct', () => {
       ],
     });
 
-    expect(results.length).toBeGreaterThan(0);
-    const foundItemNames = results.map((i) => i.name);
+    expect(results.data.length).toBeGreaterThan(0);
+    const foundItemNames = results.data.map((i) => i.name);
 
     const fishItemNames = ['Licensed Fresh Mouse', 'Sleek Cotton Table'];
 
     expect(foundItemNames).toEqual(fishItemNames);
+  });
+
+  test.skip('search with sort and pagination', async () => {
+    const results = await searchProduct(esClient, {
+      limit: 2,
+      colors: ['indigo'],
+      sort: [
+        {
+          field: 'price',
+          asc: true,
+        },
+      ],
+    });
+
+    const cursor = results.pageInfo.cursor;
+
+    const resultsNextpage = await searchProduct(esClient, {
+      limit: 2,
+      colors: ['indigo'],
+      afterCursor: cursor,
+      sort: [
+        {
+          field: 'price',
+          asc: true,
+        },
+      ],
+    });
+
+
+    expect(resultsNextpage.data.length).toBeGreaterThan(0);
+    const foundItemNames = resultsNextpage.data.map((i) => i.name);
+
+    const fishItemNames = ['Licensed Fresh Mouse', 'Sleek Cotton Table'];
+
+    expect(foundItemNames).toEqual(fishItemNames);
+  });
+});
+
+describe('cursor', () => {
+  test.each([
+    ['DFM63Pnrmz', 2000],
+    ['5J_aYxQ9yjm', undefined],
+  ])('build and Parse (%s, %i)', (a, b) => {
+    const cursor = buildCursor(a, b);
+    const { id, price } = parseCursor(cursor);
+    expect(id).toEqual(a);
+    if (b) {
+      expect(price).toEqual(b);
+    }
   });
 });
