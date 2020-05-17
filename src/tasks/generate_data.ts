9f6ee5acd5a0c4447b0d59855ed4bdd7a5560f7b
Tan Duong <tan.duong@vexere.com>
2020-05-17 12:04:38 +0700

    Generate product list

---
+++ src/tasks/generate_data.ts
@@ -0,0 +1,75 @@
+import faker from 'faker';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as shortid from 'shortid';
+
+const PRODUCT_PER_CATEGORY = 30;
+
+const brands = [1, 2, 3, 4, 5, 6, 7, 8].map(() => ({
+  id: shortid.generate(),
+  name: faker.company.companyName()
+}));
+
+const categories = [
+  {
+    id: 'food',
+    name: 'Food'
+  },
+  {
+    id: 'fashion',
+    name: 'Fashion',
+  },
+  {
+    id: 'sport',
+    name: 'Sport'
+  },
+  {
+    id: 'home-and-lifestyle',
+    name: 'Home And Lifestyle'
+  }
+];
+
+function generateProducts(category, count) {
+  console.log('category', category);
+  const products = []
+  for (let i = 0; i < count; i++) {
+    products.push({
+      id: shortid.generate(),
+      brand: faker.random.arrayElement(brands).id,
+      category: category.id,
+      name: faker.commerce.productName(),
+      color: faker.commerce.color(),
+      image: `https://i.picsum.photos/id/${Math.round(Math.random() * 1000)}/200/300.jpg`,
+      price: parseFloat(faker.commerce.price(3, 100))
+    })
+  }
+  console.log('products', products);
+  return products;
+}
+
+function generateData() {
+  let products = []
+  for (const cat of categories) {
+    products = [...products, ...generateProducts(cat, PRODUCT_PER_CATEGORY)];
+  }
+
+  fs.writeFile(path.resolve(__dirname, 'brand.json'), JSON.stringify(brands, null, 2), 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  fs.writeFile(path.resolve(__dirname, 'categories.json'), JSON.stringify(categories, null, 2), 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  fs.writeFile(path.resolve(__dirname, 'products.json'), JSON.stringify(products, null, 2), 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+generateData();
